0fa8345eb27dc81fe6b390756219755dad630b76
Mark.Zuckerberg <mark.zuckerberg@example.com>
2021-04-22 10:17:15 +0800

    Revert "Merge pull request #3 from mengjayxc/test_dev"
    This reverts commit 6cc8ffe7c1a836f57c5ac8ea6ba16b48d46be80f, reversing
    changes made to e3411262babb5b592289cc64edefee8ecb79f7dd.

--- new_db.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM follow_trade.trader_score where uuid=68285832
-
-SELECT * FROM follow_trade.trader_profit where uuid=68285832
-
-SELECT * FROM contract.multi_index_map where record_id =100585
